77c911d51cdeaa494ff9d07c20fd4ba0b9367517
lucas <lucascarvalhoptc06@gmail.com>
2026-04-19 11:35:49 -0300

    Definindo a Lògica do main

--- src/Main.java
+++ src/Main.java
@@ -2,60 +2,13 @@ import Processo.DataProcess;
 import Processo.ProcessManipulation;
 import java.util.Scanner;
 
+import static SRT.SRT.runProcess;
+
 public class Main {
-    static ProcessManipulation pr = new ProcessManipulation();
-
-    public static void runProcess() throws InterruptedException {
-        DataProcess processMoment;
-
-        System.out.println("INICIANDO O PROCESSO DE ESCALONAMENTO SRT\n");
-
-        Integer tempoAtual = 1;
-        while (!pr.hasNoProcesses()) {
-            tempoAtual++;
-            processMoment = pr.getNextProcess(tempoAtual);
-            System.out.println(
-                    "\n\n==========================  TEMPO ATUAL: " + tempoAtual + "  ==========================");
-
-            if (processMoment != null) {
-                System.out.println("\n\nPROCESSO: " + processMoment.getNome() + " - TEMPO PARA CONCLUSÃO: "
-                        + processMoment.getTempo());
-            } else {
-                System.out.println("\n\nnenhum processo em execução");
-                Thread.sleep(1000);
-                continue;
-            }
-
-            // se nulo é porque não tem proximo processo
-            if (pr.callNextProcess(tempoAtual, processMoment.getNome()) == null) {
-                pr.updateProcess(processMoment.getNome());
-
-                for (DataProcess item : pr.getAllProcess().values()) {
-
-                    if (item.getTempoChegada() < tempoAtual || item.getTempoChegada() == tempoAtual) {
-                        if (!item.getNome().equalsIgnoreCase(processMoment.getNome())
-                                && !item.getStatus().equalsIgnoreCase("F")) {
-                            item.setTempoEspera(item.getTempoEspera() + 1);
-                        }
-
-                    }
-
-                    // if (!item.getNome().equalsIgnoreCase(processMoment.getNome()) &&
-                    // !item.getStatus().equalsIgnoreCase("F")) {
-                    // item.setTempoEspera(item.getTempoEspera() + 1);
-                    // }
-                }
-
-                Thread.sleep(1000);
-            }
-
-        }
-
-        System.out.println("FIM EXECUÇÃO\n");
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ProcessManipulation pr = new ProcessManipulation();
 
         boolean sair = false;
         do {
--- src/SRT/SRT.java
+++ src/SRT/SRT.java
@@ -5,28 +5,47 @@ import Processo.ProcessManipulation;
 
 public class SRT {
 
-    ProcessManipulation processo = new ProcessManipulation();
+    static ProcessManipulation pr = new ProcessManipulation();
 
-    public void iniciarEscalonamento() {
-        int tempoTotal = 0;
 
-        while(!processo.hasNoProcesses()) {
-            DataProcess atual = processo.getNextProcess();
+    public static void runProcess() throws InterruptedException {
+        DataProcess processMoment;
 
-            if(atual != null){
+        System.out.println("INICIANDO O PROCESSO DE ESCALONAMENTO SRT\n");
 
-                System.out.println("processo atual " + atual.getNome() + "Tempo restante " + atual.getTempo());
+        Integer tempoAtual = 0;
+        while (!pr.hasNoProcesses()) {
+            tempoAtual++;
+            processMoment = pr.getNextProcess(tempoAtual);
+            System.out.println(
+                    "\n\n==========================  TEMPO ATUAL: " + tempoAtual + "  ==========================");
 
-                atual.setTempo(atual.getTempo() - 1);
-                tempoTotal++;
+            if (processMoment != null) {
+                System.out.println("\n\nPROCESSO: " + processMoment.getNome() + " - TEMPO PARA CONCLUSÃO: "
+                        + processMoment.getTempo());
+            } else {
+                System.out.println("\n\nnenhum processo em execução");
+                Thread.sleep(1000);
+                continue;
+            }
 
-                if(atual.getTempo() == 0){
-                    System.out.println("processo: " + atual.getNome() + "finalizado");
-                    processo.deleteProcessFinish(atual.getNome());
+            // se nulo é porque não tem proximo processo
+            if (pr.callNextProcess(tempoAtual, processMoment.getNome()) == null) {
+                pr.updateProcess(processMoment.getNome());
+
+                for (DataProcess item : pr.getAllProcess().values()) {
+
+                    if (item.getTempoChegada() < tempoAtual || item.getTempoChegada() == tempoAtual) {
+                        if (!item.getNome().equalsIgnoreCase(processMoment.getNome())
+                                && !item.getStatus().equalsIgnoreCase("F")) {
+                            item.setTempoEspera(item.getTempoEspera() + 1);
+                        }
+
+                    }
                 }
+                Thread.sleep(1000);
             }
         }
-
-        System.out.println("Tempo total : " + tempoTotal);
+        System.out.println("FIM EXECUÇÃO\n");
     }
 }
